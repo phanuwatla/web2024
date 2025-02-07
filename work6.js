7c9b3257de06a8ee5cc48291b16fb36ad1e033ff
phanuwatla <phanuwat.l@kkumail.com>
2025-02-08 00:24:12 +0700

    Update work6.js

--- work6.js
+++ work6.js
@@ -94,38 +94,82 @@ class App extends React.Component {
   render() {
     return (
       <Card>
-      <Card.Header>
-        <div class="header"><Alert variant="info">
-          Welcome to Work6
-          <p>เรียกใช้ฐานข้อมูล Firebase ด้วย ReactJS</p>
-        </Alert>
-        <LoginBox user={this.state.user} app={this} /></div>
-      </Card.Header>
-      <Card.Body>
+        <Card.Header>
+          <div class="header">
+            <Alert variant="info">
+              Welcome to Work6
+              <p>เรียกใช้ฐานข้อมูล Firebase ด้วย ReactJS</p>
+            </Alert>
+            <LoginBox user={this.state.user} app={this} />
+          </div>
+        </Card.Header>
+        <Card.Body>
           <div className="d-flex gap-2 btnGroup">
-            <Button onClick={() => this.readData()} className="btn btn-primary">Read Data</Button>
-            <Button onClick={() => this.autoRead()} className="btn btn-secondary">Auto Read</Button>
+            <Button onClick={() => this.readData()} className="btn btn-primary">
+              Read Data
+            </Button>
+            <Button
+              onClick={() => this.autoRead()}
+              className="btn btn-secondary"
+            >
+              Auto Read
+            </Button>
           </div>
           <div>
-          <StudentTable data={this.state.students} app={this}/>  
+            <StudentTable data={this.state.students} app={this} />
           </div>
         </Card.Body>
-      <Card.Footer>
-      <p class="addTitle">เพิ่ม/แก้ไขข้อมูล นักศึกษา</p>
-      <div className="d-flex gap-2">
-          <TextInput label="รหัสนักศึกษา" app={this} value="stdid" style={{width:120}}/>  
-          <TextInput label="คำนำหน้า" app={this} value="stdtitle" style={{width:100}} />
-          <TextInput label="ชื่อ" app={this} value="stdfname" style={{width:120}}/>
-          <TextInput label="นามสกุล" app={this} value="stdlname" style={{width:120}}/>
-          <TextInput label="Email" app={this} value="stdemail" style={{width:150}} />        
-          <TextInput label="เบอร์โทรศัพท์" app={this} value="stdphone" style={{width:120}}/>
-      </div>
-      <Button onClick={() => this.insertData()} className="btn btn-success mt-2">Save</Button>
-      </Card.Footer>
-      <Card.Footer className="text-center">
-        College of Computing, Khon Kaen University
-      </Card.Footer>
-    </Card>    
+        <Card.Footer>
+          <p class="addTitle">เพิ่ม/แก้ไขข้อมูล นักศึกษา</p>
+          <div className="d-flex gap-2">
+            <TextInput
+              label="รหัสนักศึกษา"
+              app={this}
+              value="stdid"
+              style={{ width: 120 }}
+            />
+            <TextInput
+              label="คำนำหน้า"
+              app={this}
+              value="stdtitle"
+              style={{ width: 100 }}
+            />
+            <TextInput
+              label="ชื่อ"
+              app={this}
+              value="stdfname"
+              style={{ width: 120 }}
+            />
+            <TextInput
+              label="นามสกุล"
+              app={this}
+              value="stdlname"
+              style={{ width: 120 }}
+            />
+            <TextInput
+              label="Email"
+              app={this}
+              value="stdemail"
+              style={{ width: 150 }}
+            />
+            <TextInput
+              label="เบอร์โทรศัพท์"
+              app={this}
+              value="stdphone"
+              style={{ width: 120 }}
+            />
+          </div>
+          <Button
+            onClick={() => this.insertData()}
+            className="btn btn-success mt-2"
+          >
+            Save
+          </Button>
+        </Card.Footer>
+        <Card.Footer className="text-center">
+          College of Computing, Khon Kaen University By 663380179-0 Phanuwat Lakronratch
+        </Card.Footer>
+      </Card>
     );
   }
 }
@@ -144,7 +188,10 @@ function LoginBox(props) {
     return (
       <div class="profile">
         <img src={user.photoURL} width="40" height="40" alt="profile" />
-        {user.email}
+        <div class="account">
+          <p>{user.displayName}</p>
+          <p class="email">{user.email}</p>
+        </div>
         <Button onClick={() => app.googleLogout()}>Logout</Button>
       </div>
     );
@@ -161,6 +208,7 @@ function StudentTable({ data, app }) {
           <th>ชื่อ</th>
           <th>นามสกุล</th>
           <th>Email</th>
+          <th>เบอร์โทรศัพท์</th>
           <th>Actions</th>
         </tr>
       </thead>
@@ -172,10 +220,11 @@ function StudentTable({ data, app }) {
             <td>{s.fname}</td>
             <td>{s.lname}</td>
             <td>{s.email}</td>
+            <td>{s.phone}</td>
             <td>
               <div className="d-flex gap-2">
-                <EditButton std={s} app={app}/>
-                <DeleteButton std={s} app={app}/>
+                <EditButton std={s} app={app} />
+                <DeleteButton std={s} app={app} />
               </div>
             </td>
           </tr>
@@ -204,11 +253,19 @@ function TextInput({ label, app, value, style }) {
 }
 
 function EditButton({ std, app }) {
-  return <button class="actionBtn" onClick={() => app.edit(std)}>แก้ไข</button>;
+  return (
+    <button class="actionBtn" onClick={() => app.edit(std)}>
+      <i class="fa-regular fa-pen-to-square"></i> แก้ไข
+    </button>
+  );
 }
 
 function DeleteButton({ std, app }) {
-  return <button class="actionBtn" onClick={() => app.delete(std)}>ลบ</button>;
+  return (
+    <button class="actionBtn" onClick={() => app.delete(std)}>
+      <i class="far fa-trash-alt"></i> ลบ
+    </button>
+  );
 }
 
 const container = document.getElementById("myapp");
